24a3dc41bcb046c1e451b3c10c44af570c8ba6ee
Yash Yadav <yashyadav.sr@gmail.com>
2025-05-17 17:52:57 +0530

    bugfix: implemented better URL storage

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,53 +1,12 @@
 console.log("Content Script loaded");
 
-async function getPolicyList() {
-  /**
-   * Gets the list of URLs tested for policies already. Useful so that theres not a loop
-   */
-
-  // Weirdly, implicit Promise didn't seem to run. Needed to wrap in explicit promise
-  return new Promise((resolve, reject) => {
-    chrome.storage.local.get("policyUrlsList", function (items) {
-      if (chrome.runtime.lastError) {
-        // Return errors
-        reject(chrome.runtime.lastError);
-      } else if (items.policyUrlsList === undefined) {
-        // Initialise the local storage key if it doesn't exist.
-        console.log("Initializing Policy URL List");
-
-        chrome.storage.local.set({ policyUrlsList: [] }).then((result) => {
-          console.log("Updated new value");
-        });
-
-        resolve([]);
-      } else {
-        // Return the items. This bit of code would run the most
-        console.log(items.policyUrlsList);
-        resolve(items.policyUrlsList);
-      }
-    });
-  });
-}
-
-async function updatePolicyList(value) {
-  // Adds new value to the local storage if it doesn't exist already
-  let policyUrlsList = await getPolicyList()
-  if (!policyUrlsList.includes(value)){
-
-    policyUrlsList.push(value);
-    chrome.storage.local.set({ policyUrlsList: policyUrlsList }).then(() => {
-        console.log("Updated new value");
-    });
-  }
-}
-
-function getElementsByTextContain(text) {
+function getElementsByTextContain(text, matchingElements) {
   /**
    * Returns links that may lead to privacy policy pages
    */
 
   const allElements = document.querySelectorAll("a"); // Select all links
-  const matchingElements = [];
+  //   const matchingElements = [];
 
   for (const element of allElements) {
     if (
@@ -62,19 +21,20 @@ function getElementsByTextContain(text) {
 
 (async () => {
   // Main bit of code. Calls all the functions
-  const matchingElementsPolicy = getElementsByTextContain("Policy");
-  const matchingElementsTerms = getElementsByTextContain("Terms");
+  let matchingElementsPolicy = [];
+  matchingElementsPolicy = getElementsByTextContain(
+    "Policy",
+    matchingElementsPolicy
+  );
+  matchingElementsPolicy = getElementsByTextContain(
+    "Terms",
+    matchingElementsPolicy
+  );
 
-  const policyList = await getPolicyList();
-  console.log(policyList);
-
-  if (matchingElementsPolicy.length > 0 && !location.href.includes("policy")) {
-    updatePolicyList(location.href)
-    chrome.runtime.sendMessage({
-      type: "termsFound",
-      originUrl: location.href,
-      matchingElements: [matchingElementsPolicy, matchingElementsTerms],
-      matchingElementsPolicy: matchingElementsPolicy,
-    });
-  }
+  chrome.runtime.sendMessage({
+    type: "termsFound",
+    originUrl: location.href,
+    matchingElements: [matchingElementsPolicy],
+    matchingElementsPolicy: matchingElementsPolicy,
+  });
 })();
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,7 +1,71 @@
-chrome.runtime.onMessage.addListener(function(request, sender){
-    if (request.type == "termsFound"){
-        console.log(request.originUrl);
-        console.log(request.matchingElementsPolicy)
-        // chrome.tabs.create({ url: request.matchingElementsPolicy[0]});
+chrome.storage.local.clear(function () {
+  console.log("All local storage cleared");
+});
+async function getPolicyList() {
+  /**
+   * Gets the list of URLs tested for policies already. Useful so that theres not a loop
+   */
+
+  // Weirdly, implicit Promise didn't seem to run. Needed to wrap in explicit promise
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get("policyUrlsList", function (items) {
+      if (chrome.runtime.lastError) {
+        // Return errors
+        reject(chrome.runtime.lastError);
+      } else if (items.policyUrlsList === undefined) {
+        // Initialise the local storage key if it doesn't exist.
+        console.log("Initializing Policy URL List");
+
+        chrome.storage.local.set({ policyUrlsList: {} }).then((result) => {
+          console.log("Updated new value");
+        });
+
+        resolve([]);
+      } else {
+        // Return the items. This bit of code would run the most
+        console.log(items.policyUrlsList);
+        resolve(items.policyUrlsList);
+      }
+    });
+  });
+}
+
+async function updatePolicyList(value) {
+  // Adds new value to the local storage if it doesn't exist already
+  let policyUrlsList = await getPolicyList();
+  if (!Object.keys(policyUrlsList).includes(value)) {
+    policyUrlsList[value] = {
+      verdict: "undefined",
+      last_tested: "undefined",
+    };
+
+    console.log("Updated new value");
+    console.log(policyUrlsList);
+  }
+  let confirmed = await chrome.storage.local.set({ policyUrlsList: policyUrlsList });
+  console.log(confirmed)
+  return policyUrlsList
+}
+
+async function scrapeTermsFromUrl(request) {
+  const matchingElementsPolicy = request.matchingElementsPolicy
+//   console.log(request.originUrl);
+//   console.log(matchingElementsPolicy);
+
+  let policyUrlsList = await getPolicyList()
+
+  for (const index in request.matchingElementsPolicy){
+    let url = matchingElementsPolicy[index]
+    if (!Object.keys(policyUrlsList).includes(url)){
+        policyUrlsList = await updatePolicyList(url)
     }
-})
+  } 
+  console.log(policyUrlsList)
+
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender) {
+  if (request.type == "termsFound") {
+    scrapeTermsFromUrl(request);
+  }
+});
