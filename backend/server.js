c61741a17aaf15e0130d48a27e14c477244edf63
Yash Yadav <yashyadav.sr@gmail.com>
2025-05-17 22:17:28 +0530

    re-init backend server

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send("Hello, World!")
+})
+
+app.post('/', (req, res) => {
+    res.send(req.body)
 })
 
 app.listen(port, () => {
